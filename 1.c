73b1636ee635dbbd3caa9711b01c1ac3345102bc
vishalprabha <vishal.prabhachandar1@gmail.com>
2019-01-06 14:59:13 +0530

    added edit anush 1.c

--- 1.c
+++ 1.c
@@ -3,10 +3,11 @@
 #include<stdlib.h>
 
 void main() {
-    int a[10],b[10],c[10],d[10],i;
-    for(i=0;i<10;i++)
+
+    int a[10000],b[10000],c[10000],d[10000],i;
+    for(i=0;i<10000;i++)
         a[i] = b[i] = i*10;
-    #pragma omp parallel num_threads(4)
+    #pragma omp parallel num_threads(1000)
     {
         int tid = omp_get_thread_num();
         int x;
@@ -14,15 +15,15 @@ void main() {
         {
         	#pragma omp section
         	{
-        	    printf("%d \n",omp_get_thread_num());
-        	    for(x=0;x<10;x++) {
+        	    printf("Thread no executing section 1 %d \n",omp_get_thread_num());
+        	    for(x=0;x<10000;x++) {
     	            c[x] = a[x] + b[x];
     	        }
         	}
         	#pragma omp section
         	{
-        	    printf("%d \n",omp_get_thread_num());
-        	    for(x=0;x<10;x++) {
+        	    printf("Thread no executing section 2 %d \n",omp_get_thread_num());
+        	    for(x=0;x<10000;x++) {
     	            d[x] = a[x] * b[x];
     	        }
         	}
